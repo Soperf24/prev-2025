7f1bf4dd27266d71871d87b93f0101d89d04d6b5
Soperf24 <contact@tech4work.fr>
2025-07-14 15:13:39 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 
 function App() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>PréviTPE</h1>
-      <p>Bienvenue sur l'outil d’identification des risques professionnels pour les TPE/PME.</p>
+    <div>
+      <h1>Bienvenue sur PreviTPE</h1>
+      <p>Application prête à déployer sur Vercel</p>
     </div>
   );
 }
